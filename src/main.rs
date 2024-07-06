350e29b39e7ea560828c9abecb4f7a2e6aa1358a
Mathias <mathias.yde@hotmail.dk>
2024-07-06 13:20:44 +0200

    make sudoku board with cell selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+use yew::prelude::*;
+use yew_autoprops::autoprops;
+
+#[function_component]
+fn Sudoku() -> Html {
+    let selected_cell = use_state(|| None);
+
+    let on_cell_click = {
+        let selected_cell = selected_cell.clone();
+
+        move |index: u8| {
+            log::info!("Cell {} clicked", index);
+            selected_cell.set(Some(index));
+        }
+    };
+
+    html! {
+        <div class="text-xl border border-gray-600 border-2 rounded grid grid-cols-9 grid-rows-9 gap-0" style="width: 27rem; height: 27rem;">
+            {for (0..81).map(|index| {
+                let on_cell_click = on_cell_click.clone();
+                let selected_cell = selected_cell.clone();
+
+                let is_selected = *selected_cell == Some(index);
+                let affects_selected_cell = {
+                    match *selected_cell {
+                        Some(selected_cell) => {
+                            let selected_row = selected_cell / 9;
+                            let selected_col = selected_cell % 9;
+                            let cell_row = index / 9;
+                            let cell_col = index % 9;
+
+                            selected_row == cell_row ||
+                            selected_col == cell_col ||
+                            (
+                                selected_row / 3 == cell_row / 3 &&
+                                selected_col / 3 == cell_col / 3
+                            )
+                        }
+                        None => false
+                    }
+                };
+
+                let cell_color = match (is_selected, affects_selected_cell) {
+                    (true, _) => "bg-blue-200",
+                    (_, true) => "bg-gray-200",
+                    (_, false) => "bg-gray-100",
+                };
+
+                html! {
+                    // Please allow me to write class={classes!("many classes in one string literal", cell_color)}
+                    <button onclick={move |_| on_cell_click(index)} class={cell_color}>
+                        {index}
+                    </button>
+                }
+            })}
+        </div>
+    }
+}
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div class="container mx-auto">
+            <h1 class="text-4xl font-bold text-center">{"Sudoku"}</h1>
+            <Sudoku />
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::Renderer::<App>::new().render();
 }
